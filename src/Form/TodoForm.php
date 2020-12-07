b11701d68fbcacb2f8a83e531aad4f4e685f12b7
Tyler <tyler@gravitas.net>
2020-12-07 13:22:38 +1100

    add form types to TodoForm

--- src/Form/TodoForm.php
+++ src/Form/TodoForm.php
@@ -4,6 +4,7 @@ namespace App\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,10 +15,14 @@ class TodoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('owner')
-            ->add('task')
-            ->add('status')
-            ->add('save');
+            ->add('owner', TextType::class)
+            ->add('task', TextType::class)
+            ->add('status', HiddenType::class, [
+                'data' => 'Incomplete'
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Submit Task'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
