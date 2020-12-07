9c7cc725492d4e1295bbb47e62ce5c008e6aaef1
Tyler <tyler@gravitas.net>
2020-12-07 13:12:45 +1100

    Create Task.php
    create private vars
    created public function to both gather and set all data relating to a task

---
+++ src/Entity/Task.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Entity;
+
+class todo
+{
+    private $id;
+    private $owner;
+    private $task;
+    private $status;
+
+    // Get all data relating to a task
+    public function getTodoId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTodoOwner(): ?string
+    {
+        return $this->owner;
+    }
+
+    public function getTodoTask(): ?string
+    {
+        return $this->task;
+    }
+
+    public function getTodoStatus(): ?string
+    {
+        return $this->status;
+    }
+
+
+    // Set all data relating to a task
+    public function setTodoOwner(string $owner): self
+    {
+        $this->owner = $owner;
+        return $this;
+    }
+
+    public function setTodoTask(string $task): self
+    {
+        $this->task = $task;
+        return $this;
+    }
+
+    public function setTodoStatus(string $status): self
+    {
+        $this->status = $status;
+        return $this;
+    }
+}
